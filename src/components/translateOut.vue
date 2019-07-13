494356fe17347e8ba691979fb828c1701be6cbb4
ebedoyaalzate <ebedoyaalzate@hotmail.com>
2019-07-13 12:08:54 -0500

    dojo vue

---
+++ src/components/translateOut.vue
@@ -0,0 +1,15 @@
+<template>
+<div id="translateOut">
+  <h2>{{traduciresteTexto}}</h2>
+</div>
+</template>
+
+<script>
+export default {
+  name: "translateOut",
+  props: ["traduciresteTexto"]
+};
+</script>
+
+<style>
+</style>
